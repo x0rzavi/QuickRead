72badc80aaf45edd8c3ac69746bfe79d9f85ebb9
Avishek Sen <x0rzavi@gmail.com>
2023-01-03 20:26:44 +0530

    logic: init code to scrape

--- scrape.py
+++ scrape.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 
-url = "https://startafuckingblog.com/"
+url = "https://matix.io/extract-text-from-webpage-using-beautifulsoup-and-python/"
 html = requests.get(url)
-soup = BeautifulSoup(html.text, features="lxml")
+soup = BeautifulSoup(html.text, 'lxml')
 
 text = soup.get_text()
 print(len(text.split()))
