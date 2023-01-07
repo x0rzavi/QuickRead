609c782481a7b18aa5f993c3783d79c41141eb39
Avishek Sen <x0rzavi@gmail.com>
2023-01-07 23:02:53 +0530

    logic: add health check URI

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,12 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import AnalyzeForm
 from helper.scrape import read_time
+from helper.tips import get_tips
 
 @app.route('/', methods=['GET', 'POST'])
-def root():
+def home():
     form = AnalyzeForm()
+    tip = get_tips()
     wpm = 230
     if form.validate_on_submit():
         try:
@@ -14,5 +16,9 @@ def root():
         except: 
             time = 'Error occured!'
 
-        return render_template('form.html', form=form, data=time)
-    return render_template('form.html', form=form)
+        return render_template('form.html', form=form, data=time, tips=tip)
+    return render_template('form.html', form=form, tips=tip)
+
+@app.route("/health")
+def health():
+    return "Success", 200
